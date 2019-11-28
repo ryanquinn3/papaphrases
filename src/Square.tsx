f824d197161fc4010e1523fdea8bb4681e4b1e58
Ryan Quinn <ryan3q@gmail.com>
2019-11-27 16:07:17 -0800

    color scheme

--- src/Square.tsx
+++ src/Square.tsx
@@ -19,8 +19,15 @@ const SquareContainer = styled.div`
   width: 20%;
 `;
 
-const SquareButton = styled.button<{selected: boolean}>`
-  background-color: ${ ({ selected }) => selected ? 'rgba(255,104,4, 10)': 'transparent'};  
+enum Color {
+  Green = 'rgba(179, 210, 141)',
+  OffWhite = 'rgba(235, 230, 202)',
+  Crayola = 'rgba(225, 196, 154)',
+  Champagne = 'rgba(238, 215, 168)',
+  Gold = 'rgba(235, 184, 145)'
+}
+const SquareButton = styled.button<{selected: boolean, color?: Color }>`
+  background-color: ${ ({ color }) => color };  
   color: ${ ({ selected }) => selected ? 'white': 'black'};
   position: absolute;
   left: 0;
@@ -30,17 +37,18 @@ const SquareButton = styled.button<{selected: boolean}>`
   width: 100%;
   outline: none;
   ${({ selected }) => !selected && hover}
+  
 `;
 export const Square: React.FC<Props> = ({ item }) =>  {
   const toggleItem = useStoreActions(actions => actions.toggleItem);
   const selected = useStoreState(state => {
     return state.selectedItems.includes(item.id);
   });
-
+  const color = getColor(item, selected);
     return (
       <SquareContainer>
         <SquareButton
-          disabled={item.id === freeSpace.id}
+color={color}          disabled={item.id === freeSpace.id}
           onClick={() => toggleItem(item.id)}
           selected={selected}>
           {item.value}
@@ -49,6 +57,21 @@ export const Square: React.FC<Props> = ({ item }) =>  {
       </SquareContainer>
     );
 };
+
+const getColor = (item: ContentItem, selected: boolean): Color => {
+  if(!selected) {
+    return Color.OffWhite;
+  }
+  if(item.id === freeSpace.id) {
+    return Color.Green;
+  }
+  switch(item.owner) {
+    case ContentOwner.Bill: return Color.Champagne
+    case ContentOwner.Don: return Color.Gold
+    default: return Color.Crayola
+  }
+
+}
 
 
 const Initials = styled.div`
